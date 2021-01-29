1b701f8ac1e2536219fc75514f1ffdfffe0c97fd
Geoffrey <geoffreyrobelot@gmail.com>
2021-01-29 11:45:48 +0100

    Amélioration du code

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,7 @@ class About extends React.Component {
         const titre= "A propos de moi";
         const texte= "Passionné par les nouvelles technologies et le développement informatique";
         const texte1= "Je vous invite à visiter mon dépôt Git avec mes différents projets et prendre connaissance de mon CV";
+        const texte2= "Je suis actuellement une formation à la 3W Academy afin de progresser dans les langages JavaScript et PHP";
 
         return (
             <div>
@@ -19,22 +20,23 @@ class About extends React.Component {
                         <div className="content-about">
                             <div id="un">
                                 <p>{texte}</p>
+                                <p>{texte2}</p>
                                 <p>{texte1}</p>
                             </div>
 
                             <div className="git-cv">
-                                <a href="https://github.com/geoffreyrobelot" title="Lien vers mon dépôt git">
+                                <a href="https://github.com/geoffreyrobelot" title="Lien vers mon dépôt git" target="_blank" rel="noreferrer">
                                     <img alt="Vignette de mon dépôt git"
                                         src={git}
-                                        width="18%"
-                                        height="8%" />
+                                        width="250em"
+                                        height="250em" />
                                 </a>
 
-                                <a href={cv_grand} title="Lien vers la version originale de mon CV">
+                                <a href={cv_grand} title="Lien vers la version originale de mon CV" target="_blank" rel="noreferrer">
                                     <img alt="Vignette de mon CV"
                                         src={cv}
-                                        width="15%"
-                                        height="8%" />
+                                        width="250em"
+                                        height="250em" />
                                 </a>
                                 
                             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
           {/* column 1 */}
           <div className="col1">
-            <a className="navbar-brand" href={geo} title="Lien vers ma photo en version originale"><img className="logo" src={logo} alt="logo" /></a>
+            <a className="navbar-brand" href={geo} title="Lien vers ma photo en version originale" target="_blank" rel="noreferrer"><img className="logo" src={logo} alt="logo" /></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
